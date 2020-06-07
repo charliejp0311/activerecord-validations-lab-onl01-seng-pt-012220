c3f0a85aea9a9154bacaf6cd2e6aad5affc6cf05
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-06-07 17:15:20 -0500

    IDK why this is failing LOL

--- .history/app/models/post_20200606193152.rb
+++ .history/app/models/post_20200606193152.rb
@@ -2,5 +2,4 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250} 
-    validates :category, inclusion: {within: %w(Fiction Non-Fiction), message: "%{value} is clickbait" }
 end
--- .history/app/models/post_20200607164401.rb
+++ .history/app/models/post_20200607164401.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250} 
-    validates :category, inclusion: {within: %w(Fiction Non-Fiction), message: "%{value} is clickbait" }
+    validates :category, inclusion: {within: %w("Fiction" Non-Fiction), message: "%{value} is clickbait" }
 end
